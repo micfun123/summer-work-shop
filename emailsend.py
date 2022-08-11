50aca79a273a9b4608927522f9be6653409f2ac9
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-08-11 14:33:00 +0100

    sends email

--- emailsend.py
+++ emailsend.py
@@ -13,18 +13,29 @@ PASSWORD = ""
 
 def send_mail(recipient_email):
     pdf = "receipt.pdf"
-    subject = f"receipt for {recipient_email}"
-    body = "your mail"
 
+    password = os.environ["GMAIL_PASSWORD"]
+    fromuser = os.environ["GMAIL_USER"]
+    touser = recipient_email  
+
+    subject = "your receipt"
+    body = "Your pc parts"
+    filepdf = "receipt.pdf"
     em = EmailMessage()
     em.set_content(body)
     em['From'] = fromuser
     em['To'] = recipient_email
     em['Subject'] = subject 
+    
+    with open(filepdf, 'rb') as f:
+        file_data = f.read()
+    em.add_attachment(file_data, maintype="application", subtype="pdf", filename=filepdf)
+
+
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(fromuser, PASSWORD)
+        server.login(fromuser, password)
         server.send_message(em)
         print("Email sent")
         server.quit()  
